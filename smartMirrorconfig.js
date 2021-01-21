6d0a0a3c8da9b18ac548c65b391676e9d1c76a87
Kishan1810 <68784106+Kishan1810@users.noreply.github.com>
2021-01-21 10:32:13 +0530

    Create smartMirrorconfig.js
    config file with baics changes

---
+++ smartMirrorconfig.js
@@ -0,0 +1,148 @@
+/*
+smart mirror with basic changes according to requirement
+*/
+
+var config = {
+	address: "localhost", 	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
+					// you must set the sub path here. basePath must end with a /
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+															// or add a specific IPv4 of 192.168.1.5 :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "en",
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"],
+	timeFormat: 12,
+	units: "metric",
+	// serverOnly:  true/false/"local" ,
+	// local for armv6l processors, default
+	//   starts serveronly and then starts chrome browser
+	// false, default for all NON-armv6l devices
+	// true, force serveronly mode, because you want to.. no UI on this device
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_right"
+		},
+		{
+			module: "calendar",
+			header: "Holidays",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"
+
+					},
+					{			
+
+						symbol: "my calendar",
+						url: "https://calendar.google.com/calendar/embed?src=chitrakishan123%40gmail.com&ctz=Asia%2FKolkata"
+
+
+
+					}
+				]
+			}
+		},
+		//{
+			//module: "compliments",
+			//position: "lower_third"
+		//},
+		{
+			module: "currentweather",
+			position: "top_left",
+			config: {
+				location: "Bengaluru",
+				locationID: "1277333", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "3583284ff3b401daec3a6b896cc268e4"
+			}
+		},
+		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				location: "Bengaluru",
+				locationID: "1277333", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "3583284ff3b401daec3a6b896cc268e4"
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: " Times of India",
+						url: "https://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+		{
+			  module: "MMM-NowPlayingOnSpotify",
+			  position: "top_left",
+
+			  config: {
+			    clientID: "8c40e3a2305a40ba967f6c14733b52c3",
+			    clientSecret: "ce8190548edb4c1c82ea09073b58a33a",
+			    accessToken: "BQCWoc8BOmbVH86JUwlcKhNqr45M8dHwVw98TvdF4k9ELaNrL-2TmsgXtI4l8-Vj6_DB2pC4K8sHmchTF2qHeeTlvCDYJpa8pPiXGuMpphwetaQSsDQiL23krAMHzyPBZcH4QAYcat1qsXXsuEu5dWJkEChlRIsDr5RqK06ZR6pwJg",
+			    refreshToken: "AQB-pK_e19M828eR6orW7GagxA12ARogw7L9dn-cpslgBiRjRHrONVXh535nz1wZl-70aiRRYdx_VApMs3rhCpOPA_X_WTnBuh6Q_sl5kPrN3nvRwbeAfQ51YvSa5SAZXSY"
+			  }
+			},
+	{
+    module: 'MMM-Remote-Control',
+    // uncomment the following line to show the URL of the remote control on the mirror
+    position: 'bottom_right',
+    // you can hide this module afterwards from the remote control itself
+    config: {
+        customCommand: {},  // Optional, See "Using Custom Commands" below
+        showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+        secureEndpoints: true, // Optional, See API/README.md
+        // uncomment any of the lines below if you're gonna use it
+        // customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+        // apiKey: "", // Optional, See API/README.md for details
+        // classes: {} // Optional, See "Custom Classes" below
+    }
+},
+{
+    module: "MMM-GoogleAssistant",
+    position: "top_right",
+    config: {
+        maxWidth: "100%",
+        header: "",
+    publishKey: 'YOUR_PUBNUB_PUBLISH_KEY',
+    subscribeKey: 'YOUR_PUBNUB_SUBSCRIBE_KEY',
+    updateDelay: 500
+    }
+},
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
